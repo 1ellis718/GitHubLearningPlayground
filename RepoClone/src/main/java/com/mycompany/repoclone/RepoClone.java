f472fadd3bf2270121fb7a749053eb12053088f4
1elli <1elli@LAPTOP-FOSLAG4I>
2024-09-27 12:24:42 -0500

    Initial commit

---
+++ RepoClone/src/main/java/com/mycompany/repoclone/RepoClone.java
@@ -0,0 +1,18 @@
+/*
+ * This is a simple Java program for cloning a repository.
+ */
+
+package com.mycompany.repoclone;
+
+/**
+ * RepoClone is a class that outputs a message to the console.
+ * 
+ * @author Robbie Ellis
+ */
+public class RepoClone {
+
+    public static void main(String[] args) {
+        // Output a more descriptive message to the console
+        System.out.println("Repository cloning program is ready!");
+    }
+}
